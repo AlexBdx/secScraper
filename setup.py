fcff5386cc14b3a92eee6844632ebdf87e211b08
AlexBdx <alexandre.bdx@gmail.com>
2019-10-03 06:33:56 -0400

    Test

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="secScraper",
-    version="0.0.28",
+    version="0.0.29",
     author="Alex Bondoux",
     author_email="alexandre.bdx@gmail.com",
     description="Library for Insight project",
