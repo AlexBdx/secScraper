30f64f9c98863fec28c99e70ce22c7ac05416760
AlexBdx <alexandre.bdx@gmail.com>
2019-10-10 11:28:26 -0400

    Updated the main and added the csv to postgres files

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="secScraper",
-    version="0.0.35",
+    version="0.0.36",
     author="Alex Bondoux",
     author_email="alexandre.bdx@gmail.com",
     description="Library for Insight project",
