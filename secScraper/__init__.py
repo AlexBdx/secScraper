a90e9455673ecf5c695e76d218c8027a1b0fe605
AlexBdx <alexandre.bdx@gmail.com>
2019-10-10 10:21:54 -0400

    Updated the main and added the csv to postgres files

--- secScraper/__init__.py
+++ secScraper/__init__.py
@@ -7,11 +7,12 @@ module_blacklist = [
     '__init__.py',
     'setup.py',
     'Filing metrics.py',
-    'main.py',
-    'ec2_scraping.py'
+    'main_new_scores.py',
+    'ec2_scraping.py',
+    'csv_to_postgres.py'
 ]
 
 __name__ = "secScraper"
-__version__ = "0.0.31"
+__version__ = "0.0.32"
 __all__ = [ basename(f)[:-3] for f in modules if isfile(f) and split(f)[1] not in module_blacklist]
 # print("[INFO] Loaded the following modules:\n", __all__)
